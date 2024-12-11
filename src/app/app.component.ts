fde791e7720f1ec9f049e2169e08c4592e175311
sohilametwaly <sohailarafat934@yahoo.com>
2024-12-11 21:20:32 +0200

    add card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavBarComponent } from '../Components/nav-bar/nav-bar.component';
+import { CourseCardComponent } from '../Components/course-card/course-card.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, CourseCardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
