dff3c0372cb0a4aca9ab132e8f9b94f20c92c3e7
engy medhat <mengy289@gmail.com>
2024-12-11 21:36:21 +0200

    grading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavBarComponent } from '../Components/nav-bar/nav-bar.component';
-import { CourseCardComponent } from '../Components/course-card/course-card.component';
+import { GradingFormComponent } from './grading/grading.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent, CourseCardComponent],
+  imports: [RouterOutlet, GradingFormComponent, ToastrModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
