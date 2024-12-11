0f259af6c96d5c72450f1a6a1040162dfb0fd26f
engy medhat <mengy289@gmail.com>
2024-12-11 21:36:21 +0200

    grading page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GradingFormComponent } from './grading/grading.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, GradingFormComponent, ToastrModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'E-Learning-Project';
